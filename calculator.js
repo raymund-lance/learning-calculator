8e711656da947113748eb1f47608a719679e88c0
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-22 19:56:19 +0800

    add function to get user input and operator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let firstInput = 0;
+let secondInput = 0;
+let operator;
+
 function addNumbers(numOne, numTwo) {
     return numOne + numTwo;
 }
@@ -14,4 +18,25 @@ function divideNumbers(numOne, numTwo) {
     return numOne / numTwo;
 }
 
-console.log(addNumbers(1,2));
+function solve() {
+    firstInput = prompt("input first number");
+    operator = prompt("Enter operator + - * /");
+    secondInput = prompt("Enter second number");
+    for(let i = 0; i < 4; i++) {
+        switch(operator) {
+            case '+':
+                console.log(addNumbers(firstInput, secondInput));
+                break;
+            case '-':
+                console.log(subtractNumbers(firstInput, secondInput));
+                break;
+            case '*':
+                console.log(multiplyNumbers(firstInput, secondInput));
+                break;
+            case '/':
+                console.log(divideNumbers(firstInput, secondInput));
+                break
+        }
+    }
+}
+solve();
