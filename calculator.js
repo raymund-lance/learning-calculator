63975de6cd1b9cde1a8896fa88f97a9450cde6af
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-22 19:29:37 +0800

    done making the operator functions

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function addNumbers(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtractNumbers(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiplyNumbers(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divideNumbers(numOne, numTwo) {
+    return numOne / numTwo;
+}
+
+console.log(addNumbers(1,2));
