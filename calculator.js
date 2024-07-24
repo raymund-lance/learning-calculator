96c57a2bf7022d42c6187ac9e837ad29012aa88c
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-24 19:00:31 +0800

    manage to display first number then operator then second number in screen

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 let firstInput = 0;
 let secondInput = 0;
-let operator;
+const getDisplay = document.querySelector('#screen');
 
 function addNumbers(numOne, numTwo) {
     return +numOne + +numTwo;
@@ -18,24 +18,58 @@ function divideNumbers(numOne, numTwo) {
     return numOne / numTwo;
 }
 
-function solve() {
-    firstInput = prompt("input first number");
-    operator = prompt("Enter operator + - * /");
-    secondInput = prompt("Enter second number");
-    for(let i = 0; i < 4; i++) {
-        switch(operator) {
-            case '+':
-                console.log(addNumbers(firstInput, secondInput));
-                break;
-            case '-':
-                console.log(subtractNumbers(firstInput, secondInput));
-                break;
-            case '*':
-                console.log(multiplyNumbers(firstInput, secondInput));
-                break;
-            case '/':
-                console.log(divideNumbers(firstInput, secondInput));
-                break
-        }
-    }
+// function solve(firstInput, secondInput) {
+//     for(let i = 0; i < 4; i++) {
+//         switch(operator) {
+//             case '+':
+//                 getDisplay.textContent = addNumbers(firstInput, secondInput);
+//                 break;
+//             case '-':
+//                 console.log(subtractNumbers(firstInput, secondInput));
+//                 break;
+//             case '*':
+//                 console.log(multiplyNumbers(firstInput, secondInput));
+//                 break;
+//             case '/':
+//                 console.log(divideNumbers(firstInput, secondInput));
+//                 break
+//         }
+//     }
+// }
+
+function getFirstNumbers(firstNumberValue) {
+    console.log(firstNumberValue);
+    return firstNumberValue;
 }
+const buttonNumbers = document.querySelectorAll('.numberButton');
+const buttonOperator = document.querySelectorAll('.operatorButton');
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let dummyOperator = "";
+
+buttonNumbers.forEach(numberButton => {
+    numberButton.addEventListener('click', function() {
+        if(operator == "") {
+        const numberOneValue = this.value;
+        firstNumber += numberOneValue;
+        getDisplay.textContent = firstNumber;
+        } else if(operator !== dummyOperator) {
+            const numberTwoValue = this.value;
+            secondNumber += numberTwoValue;
+            getDisplay.textContent = firstNumber + operator + secondNumber;
+        }
+    })
+})
+
+
+buttonOperator.forEach(operatorButton => {
+    operatorButton.addEventListener('click',function() {
+        const operatorValue = this.value;
+        operator = operatorValue
+        getDisplay.textContent = firstNumber + operator;
+    })
+})
+
+
